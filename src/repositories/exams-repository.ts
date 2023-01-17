625f9d07bc35e840ef98c7156f60198687a7993c
Starkov Sergey <starkovsr@gmail.com>
2023-01-17 23:53:27 +0300

    feat: add minimal questions quantity for GET exams request

--- src/repositories/exams-repository.ts
+++ src/repositories/exams-repository.ts
@@ -2,9 +2,11 @@ import { exams } from '../exams'
 import { shuffleArray } from '../utils/shuffle-array'
 import { questions } from '../questions'
 
+const MINIMAL_QUESTIONS_QUANTITY = 5
+
 export const examsRepository = {
   getExams() {
-    return exams
+    return exams.filter(exam => questions[exam.id].length >= MINIMAL_QUESTIONS_QUANTITY)
   },
   getExamQuestionsById(id: string) {
     return shuffleArray(questions[id]).slice(0, 10)
